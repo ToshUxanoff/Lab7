a18c659f4cf726215b78194b29fc7d21f7e035a2
Уханов Антон <tosha.uhanoff@yandex.ru>
2017-02-27 18:50:00 +0300

    Update (Fixed)Part 2.cpp

--- (Fixed)Part 2.cpp
+++ (Fixed)Part 2.cpp
@@ -4,7 +4,7 @@
 #include "fstream"
 #include "cctype"
 
-void Context(std::string* buf, int * position, int founded, int context_words, int words)
+void Context(std::string* word, int * position, int founded, int context_words, int words)
 {
 	for (int j = 0; j < founded; j++)	
 	{
@@ -14,21 +14,24 @@ void Context(std::string* buf, int * position, int founded, int context_words, i
 			{
 				continue;
 			}
-			std::cout << buf[position[j] - context_words + i] << ' ';
-			if (buf[position[j] - context_words + i] == buf[words - 1]) //Если ищется контекст для последнего элемента справа - брейк
+			else if (word[position[j] - context_words + i] == word[words - 1]) //Если ищется контекст для последнего элемента справа - брейк
 			{
 				break;
 			}
+			else
+			{
+				std::cout << word[(position[j] - context_words) + i] << " ";
+			}
 		}
-		std::cout << "\n\n\n";
+		std::cout << std::endl;
 	}
 }
-int Search(std::ifstream& File, std::string *buf, std::string str, int words)
+int Search(std::ifstream& File, std::string *word, std::string str, int words)
 {
 	int founded = 0;
 	for (int i = 0; i < words; i++)
 	{
-		if (buf[i] == str)
+		if (word[i] == str)
 		{
 			founded++;
 		}
@@ -37,20 +40,16 @@ int Search(std::ifstream& File, std::string *buf, std::string str, int words)
 }
 int FileSize(std::ifstream& File)
 {
-	char ch = 0;
 	int words = 0;
 	std::string endfile;
 	while (!File.eof())
 	{
-		File.get(ch);
-		if (std::isalnum(ch) == false)
-		{
-			words++;
-		}
+		File >> endfile;
+		words++;
 	}
 	File.clear();
 	File.seekg(0);
-	return words;
+	return words+1;
 }
 std::string* Read(std::ifstream& File, std::string* word, int words)
 {
@@ -63,6 +62,11 @@ std::string* Read(std::ifstream& File, std::string* word, int words)
 			File.get(ch);
 			if (std::isalnum(ch) == false)
 			{
+				do
+				{
+					File.get(ch);
+				} while (std::isalnum(ch) == false);
+				File.unget();
 				break;
 			}
 			else
@@ -70,7 +74,7 @@ std::string* Read(std::ifstream& File, std::string* word, int words)
 				buffer += ch;
 			}
 		}
-		word[i] = buffer;
+		word[i] = buffer;	
 		buffer.erase(buffer.begin(), buffer.end());
 	}
 	return word;
@@ -79,20 +83,21 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	std::ifstream File("FileTest.txt");
 	int words = FileSize(File); //Количество слов
-	std::string *buf = new std::string[words];
+	std::string *word = new std::string[words];
 	std::string str;
-	Read(File, buf, words);
+	Read(File, word, words);
 	std::cout << "Keyword : ";
 	std::cin >> str;
-	int founded = Search(File, buf, str, words);
+	int founded = Search(File, word, str, words);
 	int* position = new int[founded];
 	int j = 0;
 	for (int i = 0; i < words; i++)
 	{										
-		if (buf[i] == str)		
+		if (word[i] == str)		
 		{
 			position[j] = i;
 			j++;
+			std::cout << i << ' ';
 		}
 	}
 	if (founded != 0)
@@ -101,15 +106,18 @@ int _tmain(int argc, _TCHAR* argv[])
 		std::cout << "\n Number of context words: ";
 		int context_words = 0;
 		std::cin >> context_words;
-		Context(buf, position, founded, context_words, words);
+		Context(word, position, founded, context_words, words);
 	}
 	else
 	{
 		std::cout << "Word was not founded. Context does not exist!" <<std::endl;
 	}
-	delete[]buf;
+	delete[]word;
 	delete[]position;
 	system("pause");
 	return 0;
+}
+
+
 }
 
